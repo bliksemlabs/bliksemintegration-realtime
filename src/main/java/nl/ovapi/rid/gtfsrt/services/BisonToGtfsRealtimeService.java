e4135f2776ff98bd333f06a474cab20ef3a9c310
Thomas Koch <thakoch@gmail.com>
2014-01-02 23:13:04 +0100

    Seperate queue to create elastic buffer recieving KV6 data

--- src/main/java/nl/ovapi/rid/gtfsrt/services/BisonToGtfsRealtimeService.java
+++ src/main/java/nl/ovapi/rid/gtfsrt/services/BisonToGtfsRealtimeService.java
@@ -230,6 +230,7 @@ public class BisonToGtfsRealtimeService {
 		_executor = Executors.newCachedThreadPool();
 		_scheduler = Executors.newScheduledThreadPool(5);
 		_scheduler.scheduleAtFixedRate(new GarbageCollectorTask(), 60, GARBAGE_COLLECTOR_INTERVAL_SECONDS, TimeUnit.SECONDS);
+		_task = _executor.submit(new ProcessTask());
 		_task = _executor.submit(new ReceiveTask());
 		try {
 			_executor.submit(new ProcessKV15Task(_ridService.getActiveKV15messages()));
@@ -562,7 +563,7 @@ public class BisonToGtfsRealtimeService {
 		_executor.submit(new ProcessKV6Task(posinfos));
 	}
 
-	private class ReceiveTask implements Runnable {
+	private class ProcessTask implements Runnable {
 		int messagecounter = 0;
 		@Override
 		public void run() {
@@ -573,65 +574,90 @@ public class BisonToGtfsRealtimeService {
 			try {sp = spf.newSAXParser();
 			xr = sp.getXMLReader();} catch (Exception e) {return;}
 			Context context = ZMQ.context(1);
+			Socket pull = context.socket(ZMQ.PULL);
+			pull.setRcvHWM(500000);
+			final String PULL_ADDRESS = "tcp://127.0.0.1:"+INPROC_PORT;
+			pull.connect(PULL_ADDRESS);
+			while (!Thread.interrupted()) {
+				messagecounter++;
+				if (messagecounter % 1000 == 0){
+					_log.debug(messagecounter + " BISON messages received");
+				}
+				try {
+					String[] m = ZeroMQUtils.gunzipMultifameZMsg(ZMsg.recvMsg(pull));
+					if (m[0].toLowerCase().endsWith("kv6posinfo")) {
+						InputSource s = new InputSource(new StringReader(m[1]));
+						KV6SAXHandler handler = new KV6SAXHandler();
+						xr.setContentHandler(handler);
+						try {
+							xr.parse(s);
+							process(handler.getPosinfos());
+						} catch (Exception e) {
+							_log.error("KV6 parsing {}",m[1],e);
+						}
+					} else if (m[0].toLowerCase().endsWith("kv17cvlinfo")) {
+						InputSource s = new InputSource(new StringReader(m[1]));
+						KV17SAXHandler handler = new KV17SAXHandler();
+						xr.setContentHandler(handler);
+						try {
+							xr.parse(s);
+							_executor.submit(new ProcessKV17Task(handler.getCvlinfos()));
+							System.out.println(m[1]);
+						} catch (Exception e) {
+							_log.error("KV17 parsing {}",m[1],e);
+						}
+					} else if (m[0].toLowerCase().endsWith("kv15messages")) {
+						InputSource s = new InputSource(new StringReader(m[1]));
+						KV15SAXHandler handler = new KV15SAXHandler();
+						xr.setContentHandler(handler);
+						try {
+							xr.parse(s);
+							_executor.submit(new ProcessKV15Task(handler.getMessages()));
+						} catch (Exception e) {
+							_log.error("KV15 parsing {}",m[1]);
+						}
+					} else {
+						_log.error("Unknown URL {}",m[0]);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}	
+			}
+			_log.error("BisonToGtfsRealtime service interrupted");
+			pull.disconnect(PULL_ADDRESS);
+		}
+	}
+
+	private final static String INPROC_PORT = "51545";
+
+	private class ReceiveTask implements Runnable {
+		@Override
+		public void run() {
+			Context context = ZMQ.context(1);
 			Socket subscriber = context.socket(ZMQ.SUB);
 			subscriber.connect(pubAdress);
 			subscriber.subscribe("".getBytes());
+			Socket push = context.socket(ZMQ.PUSH);
+			push.setSndHWM(500000);
+			push.bind("tcp://*:"+INPROC_PORT);
+			_log.info("Connect to {}",pubAdress);
 			org.zeromq.ZMQ.Poller poller = context.poller();
 			poller.register(subscriber);
 			while (!Thread.interrupted()) {
-				if (poller.poll(60*1000*5) > 0){
-					messagecounter++;
-					if (messagecounter % 1000 == 0){
-						_log.debug(messagecounter + " BISON messages received");
-					}
-					try {
-						String[] m = ZeroMQUtils.gunzipMultifameZMsg(ZMsg.recvMsg(subscriber));
-						m = ZeroMQUtils.gunzipMultifameZMsg(ZMsg.recvMsg(subscriber));
-						if (m[0].toLowerCase().endsWith("kv6posinfo")) {
-							InputSource s = new InputSource(new StringReader(m[1]));
-							KV6SAXHandler handler = new KV6SAXHandler();
-							xr.setContentHandler(handler);
-							try {
-								xr.parse(s);
-								process(handler.getPosinfos());
-							} catch (Exception e) {
-								_log.error("KV6 parsing {}",m[1],e);
-							}
-						} else if (m[0].toLowerCase().endsWith("kv17cvlinfo")) {
-							InputSource s = new InputSource(new StringReader(m[1]));
-							KV17SAXHandler handler = new KV17SAXHandler();
-							xr.setContentHandler(handler);
-							try {
-								xr.parse(s);
-								_executor.submit(new ProcessKV17Task(handler.getCvlinfos()));
-								System.out.println(m[1]);
-							} catch (Exception e) {
-								_log.error("KV17 parsing {}",m[1],e);
-							}
-						} else if (m[0].toLowerCase().endsWith("kv15messages")) {
-							InputSource s = new InputSource(new StringReader(m[1]));
-							KV15SAXHandler handler = new KV15SAXHandler();
-							xr.setContentHandler(handler);
-							try {
-								xr.parse(s);
-								_executor.submit(new ProcessKV15Task(handler.getMessages()));
-							} catch (Exception e) {
-								_log.error("KV15 parsing {}",m[1]);
-							}
-						} else {
-							_log.error("Unknown URL {}",m[0]);
-						}
-					} catch (IOException e) {
+				if (poller.poll(TimeUnit.MINUTES.toMillis(5L)) > 0){
+					try{
+						ZMsg.recvMsg(subscriber).send(push);
+					} catch (Exception e) {
+						_log.error("Error in bison receiving",e);
 						e.printStackTrace();
-					}	
+					}
 				}else{
 					subscriber.disconnect(pubAdress);
-					_log.error("Connection to {} lost, reconnecting",pubAdress);
 					subscriber.connect(pubAdress);
+					_log.error("Connection to {} lost, reconnecting",pubAdress);
 					subscriber.subscribe("".getBytes());
 				}
 			}
-			_log.error("BisonToGtfsRealtime service interrupted");
 			subscriber.disconnect(pubAdress);
 		}
 	}
