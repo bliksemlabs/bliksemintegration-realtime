eef32a69f1c68cf17e212fb6fb6d7e3c18bd22b4
Thomas Koch <thakoch@gmail.com>
2014-02-10 12:22:12 +0100

    Improve javadoc

--- src/main/java/nl/ovapi/rid/model/JourneyPattern.java
+++ src/main/java/nl/ovapi/rid/model/JourneyPattern.java
@@ -2,41 +2,76 @@ package nl.ovapi.rid.model;
 
 import java.util.ArrayList;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import lombok.ToString;
 
 @ToString()
+@EqualsAndHashCode()
 public class JourneyPattern {
 
 	@Getter
 	@Setter
+	/**
+	 * DirectionType of JourneyPattern within Line. 1 or 2.
+	 */
 	public Integer directiontype;
 	
 	@ToString()
 	public static class JourneyPatternPoint {
 		@Getter
 		@Setter
+		@NonNull
+		/**
+		 * Sequential order in JourneyPattern and TimeDemandGroup
+		 */
 		private Integer pointorder;
 		@Getter
 		@Setter
+		@NonNull
+		/**
+		 * Reference to this point in RID database.
+		 */
 		private Long pointref;
 		@Getter
 		@Setter
+		@NonNull
+		/**
+		 * DataOwnerCode:UserStopCode of StopPoint.
+		 */
 		private String operatorpointref;
 		@Getter
 		@Setter
+		@NonNull
+		/**
+		 * Whether this point is a WaitPoint/TimingPoint, if true a vehicle is not expected to depart early from this stop
+		 */
 		public boolean waitpoint;
 		@Getter
 		@Setter
+		@NonNull
+		/**
+		 * Distance in meters from start of route. NOTE: does not have to start at 0.
+		 */
 		private Integer distancefromstartroute;
 		@Getter
 		@Setter
+		@NonNull
+		/**
+		 * If stoppoint is not Scheduled, its a dummy not meant for passengers such as bridges and KAR points.
+		 * Dummies are included in the case a KV6 messages arrives on a dummy.
+		 */
 		private boolean scheduled;
 
 	}
 	
-	public boolean contains(String userstopcode) {
+	/**
+	 * @param userstopcode DataOwnerCode':'UserStopCode
+	 * @return false (JourneyPattern does not contain point), true (JourneyPattern contains point)
+	 */
+	public boolean contains(@NonNull String userstopcode) {
 		for (JourneyPatternPoint pt : points) {
 			if (userstopcode.equals(pt.getOperatorpointref())) {
 				return true;
@@ -45,17 +80,31 @@ public class JourneyPattern {
 		return false;
 	}
 	
-	public JourneyPatternPoint getPoint(int pointorder){
+	/**
+	 * 
+	 * @param pointOrder Sequential order to be retrievd
+	 * @return NULL: PointOrder not present in JourneyPattern
+	 *         StopPoint with given PointOrder
+	 */
+	
+	public JourneyPatternPoint getPoint(int pointOrder){
 		for (JourneyPatternPoint pt : points){
-			if (pt.pointorder.equals(pointorder)){
+			if (pt.pointorder.equals(pointOrder)){
 				return pt;
 			}
 		}
 		return null;
 	}
-
+	
+	/**
+	 * List with StopPoints in JourneyPattern
+	 */
 	@Getter private ArrayList<JourneyPatternPoint> points;
 	
+	/**
+	 * Add point to JourneyPattern
+	 * @param point
+	 */
 	public void add(JourneyPatternPoint point){
 		points.add(point);
 	}
--- src/main/java/nl/ovapi/rid/model/TimeDemandGroup.java
+++ src/main/java/nl/ovapi/rid/model/TimeDemandGroup.java
@@ -2,27 +2,45 @@ package nl.ovapi.rid.model;
 
 import java.util.ArrayList;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @ToString(callSuper = true)
+@EqualsAndHashCode()
 public class TimeDemandGroup {
 
 	public static class TimeDemandGroupPoint {
 		@Getter
 		@Setter
+		/**
+		 * Sequential order in JourneyPattern and TimeDemandGroup
+		 */
 		public Integer pointorder;
 		@Getter
 		@Setter
+		/**
+		 * Seconds since start of trip
+		 */
 		public Integer totaldrivetime;
 		@Getter
 		@Setter
+		/**
+		 * Seconds between arrivaltime and departuretime at this stop.
+		 */
 		public Integer stopwaittime;
 	}
 
+	/**
+	 * List of points in TimeDemandGroup
+	 */
 	@Getter private ArrayList<TimeDemandGroupPoint> points;
 
+	/**
+	 * Add point to TimeDemandGroup
+	 * @param point
+	 */
 	public void add(TimeDemandGroupPoint point){
 		points.add(point);
 	}
