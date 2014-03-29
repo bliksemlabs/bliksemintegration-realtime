4c2550ed5ce2e57cfcf4a7259f93262687eb86b6
Thomas Koch <thakoch@gmail.com>
2014-03-29 13:35:39 +0100

    Try first to get GVB journey direct then try mapping

--- src/main/java/nl/ovapi/rid/gtfsrt/services/KV78TurboToPseudoKV6Service.java
+++ src/main/java/nl/ovapi/rid/gtfsrt/services/KV78TurboToPseudoKV6Service.java
@@ -114,15 +114,16 @@ public class KV78TurboToPseudoKV6Service {
 
 	private KV6posinfo makePseudoKV6(DatedPasstime pt){
 		String id = String.format("%s:%s:%s:%s", pt.getOperationDate(),pt.getDataOwnerCode(),pt.getLinePlanningNumber(),pt.getJourneyNumber());
-		if (pt.getDataOwnerCode() == DataOwnerCode.GVB){
+		Journey j = _ridService.getJourney(id);
+		if (j == null && pt.getDataOwnerCode() == DataOwnerCode.GVB){
 			String newId = _ridService.getGVBdeltaId(id);
+			j = _ridService.getJourney(id);
 			if (newId != null){
 				id = newId;
 			}else{
 				_log.info("GVB delta ID not found {}",id);
 			}
 		}
-		Journey j = _ridService.getJourney(id);
 		if (j == null){
 			return null;
 		}
