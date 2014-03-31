8e20b6c2f28b549129ab1f310daa9b5fffc571b4
Thomas Koch <thakoch@gmail.com>
2014-03-31 16:00:13 +0200

    Skip seconds if not necessary

--- src/main/java/nl/tt_solutions/schemas/ns/rti/_1/DelayAdapter.java
+++ src/main/java/nl/tt_solutions/schemas/ns/rti/_1/DelayAdapter.java
@@ -5,21 +5,23 @@ import javax.xml.datatype.Duration;
 
 public class DelayAdapter extends XmlAdapter<String, Duration> {
 
-    @Override
-    public String marshal(Duration v) throws Exception {
-    	int seconds = v.getDays()*60*60*24+v.getHours()*60*60+v.getMinutes()*60+v.getSeconds();
-    	StringBuilder sb = new StringBuilder();
-    	sb.append("PT");
-    	sb.append(seconds/60);
-    	sb.append("M");
-    	sb.append(seconds%60);
-    	sb.append("S");
-    	return sb.toString();
-    }
+	@Override
+	public String marshal(Duration v) throws Exception {
+		int seconds = v.getDays()*60*60*24+v.getHours()*60*60+v.getMinutes()*60+v.getSeconds();
+		StringBuilder sb = new StringBuilder();
+		sb.append("PT");
+		sb.append(seconds/60);
+		sb.append("M");
+		if (seconds%60 > 0){
+			sb.append(seconds%60);
+			sb.append("S");
+		}
+		return sb.toString();
+	}
 
-    @Override
-    public Duration unmarshal(String v) throws Exception {
-    	return DatatypeFactory.newInstance().newDuration(v);
-    }
+	@Override
+	public Duration unmarshal(String v) throws Exception {
+		return DatatypeFactory.newInstance().newDuration(v);
+	}
 
 }
