0a863df501b55e2c90bf9f53808ba7c53bceeb24
Thomas Koch <thakoch@gmail.com>
2014-04-18 13:02:43 +0200

    Fix updatetimestamp in seconds instead of longs

--- src/main/java/nl/ovapi/bison/model/DatedPasstime.java
+++ src/main/java/nl/ovapi/bison/model/DatedPasstime.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import nl.ovapi.bison.DateUtils;
+import nl.ovapi.rid.gtfsrt.Utils;
 
 import com.google.common.base.Objects;
 
@@ -70,98 +71,98 @@ public class DatedPasstime {
 
 	public void setForBoarding(boolean forBoarding) {
 		if (!Objects.equal(forBoarding, this.forBoarding)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.forBoarding = forBoarding;
 	}
 
 	public void setForAlighting(boolean forAlighting) {
 		if (!Objects.equal(forAlighting, this.forAlighting)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.forAlighting = forAlighting;
 	}
 
 	public void setReasonType(String reasonType) {
 		if (!Objects.equal(reasonType, this.reasonType)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.reasonType = reasonType;
 	}
 
 	public void setOperatorCode(String operatorCode) {
 		if (!Objects.equal(operatorCode, this.operatorCode)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.operatorCode = operatorCode;
 	}
 
 	public void setWheelChairAccessible(WheelChairAccessible wheelChairAccessible) {
 		if (!Objects.equal(wheelChairAccessible, this.wheelChairAccessible)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.wheelChairAccessible = wheelChairAccessible;
 	}
 
 	public void setNumberOfCoaches(Integer numberOfCoaches) {
 		if (!Objects.equal(numberOfCoaches, this.numberOfCoaches)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.numberOfCoaches = numberOfCoaches;
 	}
 
 	public void setSideCode(String sideCode) {
 		if (!Objects.equal(sideCode, this.sideCode)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.sideCode = sideCode;
 	}
 
 	public void setMessageType(MessageType messageType) {
 		if (!Objects.equal(messageType, this.messageType)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.messageType = messageType;
 	}
 
 	public void setMessageContent(String messageContent) {
 		if (!Objects.equal(messageContent, this.messageContent)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.messageContent = messageContent;
 	}
 
 	public void setTripStopStatus(TripStopStatus tripStopStatus) {
 		if (!Objects.equal(tripStopStatus, this.tripStopStatus)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.tripStopStatus = tripStopStatus;
 	}
 
 	public void setExpectedDepartureTime(Integer expectedDepartureTime) {
 		if (!Objects.equal(expectedDepartureTime, this.expectedDepartureTime)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.expectedDepartureTime = expectedDepartureTime;
 	}
 
 	public void setExpectedArrivalTime(Integer expectedArrivalTime) {
 		if (!Objects.equal(expectedArrivalTime, this.expectedArrivalTime)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.expectedArrivalTime = expectedArrivalTime;
 	}
 
 	public void setTimingStop(boolean isTimingStop) {
 		if (!Objects.equal(isTimingStop, this.isTimingStop)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.isTimingStop = isTimingStop;
 	}
 
 	public void setDestinationCode(String destinationCode) {
 		if (!Objects.equal(destinationCode, this.destinationCode)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.destinationCode = destinationCode;
 	}
@@ -172,154 +173,154 @@ public class DatedPasstime {
 
 	public void setLineDirection(Integer lineDirection) {
 		if (!Objects.equal(lineDirection, this.lineDirection)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.lineDirection = lineDirection;
 	}
 
 	public void setJourneyPatternCode(Integer journeyPatternCode) {
 		if (!Objects.equal(journeyPatternCode, this.journeyPatternCode)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.journeyPatternCode = journeyPatternCode;
 	}
 
 	public void setLocalServiceLevelCode(Integer localServiceLevelCode) {
 		if (!Objects.equal(localServiceLevelCode, this.localServiceLevelCode)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.localServiceLevelCode = localServiceLevelCode;
 	}
 
 	public void setUserStopCode(String userStopCode) {
 		if (!Objects.equal(userStopCode, this.userStopCode)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.userStopCode = userStopCode;
 	}
 
 	public void setUserStopOrderNumber(Integer userStopOrderNumber) {
 		if (!Objects.equal(userStopOrderNumber, this.userStopOrderNumber)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.userStopOrderNumber = userStopOrderNumber;
 	}
 
 	public void setFortifyOrderNumber(Integer fortifyOrderNumber) {
 		if (!Objects.equal(fortifyOrderNumber, this.fortifyOrderNumber)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.fortifyOrderNumber = fortifyOrderNumber;
 	}
 
 	public void setJourneyNumber(Integer journeyNumber) {
 		if (!Objects.equal(journeyNumber, this.journeyNumber)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.journeyNumber = journeyNumber;
 	}
 
 	public void setLinePlanningNumber(String linePlanningNumber) {
 		if (!Objects.equal(linePlanningNumber, this.linePlanningNumber)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.linePlanningNumber = linePlanningNumber;
 	}
 
 	public void setOperationDate(String operationDate) {
 		if (!Objects.equal(operationDate, this.operationDate)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.operationDate = operationDate;
 	}
 
 	public void setDataOwnerCode(DataOwnerCode dataOwnerCode) {
 		if (!Objects.equal(dataOwnerCode, this.dataOwnerCode)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.dataOwnerCode = dataOwnerCode;
 	}
 
 	public void setSubReasonType(String subReasonType) {
 		if (!Objects.equal(subReasonType, this.subReasonType)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.subReasonType = subReasonType;
 	}
 
 	public void setReasonContent(String reasonContent){
 		if (!Objects.equal(reasonContent, this.reasonContent)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.reasonContent = reasonContent;
 	}
 
 	public void setAdviceType(String adviceType){
 		if (!Objects.equal(adviceType, this.adviceType)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.adviceType = adviceType;
 	}	
 
 	public void setSubAdviceType(String subAdviceType){
 		if (!Objects.equal(subAdviceType, this.subAdviceType)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.subAdviceType = subAdviceType;
 	}	
 
 	public void setAdviceContent(String adviceContent){
 		if (!Objects.equal(adviceContent, this.adviceContent)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.adviceContent = adviceContent;
 	}	
 
 	public void setTimingPointDataOwnerCode(DataOwnerCode timingPointDataOwnerCode){
 		if (!Objects.equal(timingPointDataOwnerCode, this.timingPointDataOwnerCode)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.timingPointDataOwnerCode = timingPointDataOwnerCode;
 	}
 
 	public void setTimingPointCode(String timingPointCode){
 		if (!Objects.equal(timingPointCode, this.timingPointCode)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.timingPointCode = timingPointCode;
 	}
 
 	public void setJourneyStopType(JourneyStopType journeyStopType){
 		if (!Objects.equal(journeyStopType, this.journeyStopType)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.journeyStopType = journeyStopType;
 	}
 
 	public void setTargetDepartureTime(Integer targetDepartureTime){
 		if (!Objects.equal(targetDepartureTime, this.targetDepartureTime)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.targetDepartureTime = targetDepartureTime;
 	}
 
 	public void setTargetArrivalTime(Integer targetArrivalTime){
 		if (!Objects.equal(targetArrivalTime, this.targetArrivalTime)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.targetArrivalTime = targetArrivalTime;
 	}
 
 	public void setRecordedDepartureTime(Integer recordedDepartureTime){
 		if (!Objects.equal(recordedDepartureTime, this.recordedDepartureTime)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.recordedDepartureTime = recordedDepartureTime;
 	}
 
 	public void setRecordedArrivalTime(Integer recordedArrivalTime){
 		if (!Objects.equal(recordedArrivalTime, this.recordedArrivalTime)){
-			this.setLastUpdateTimeStamp(System.currentTimeMillis());
+			this.setLastUpdateTimeStamp(Utils.currentTimeSecs());
 		}
 		this.recordedArrivalTime = recordedArrivalTime;
 	}
@@ -357,7 +358,7 @@ public class DatedPasstime {
 		sb.append(journeyPatternCode).append('|');
 		sb.append(lineDirection).append('|');
 		SimpleDateFormat iso8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-		sb.append(iso8601.format(new Date(lastUpdateTimeStamp))).append('|');
+		sb.append(iso8601.format(new Date(lastUpdateTimeStamp*1000))).append('|');
 		sb.append(destinationCode).append('|');
 		sb.append(isTimingStop ? 1 : 0).append('|');
 		String eta = to32Time(expectedArrivalTime);
