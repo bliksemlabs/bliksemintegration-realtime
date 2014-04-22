94ca4159b90474fe5273c2f9470db0a9ee392d6e
Thomas Koch <thakoch@gmail.com>
2014-04-22 09:37:39 +0200

    Enable ZMq exporter of ARNU ritinfO

--- src/main/java/nl/ovapi/arnu/ARNUexporter.java
+++ src/main/java/nl/ovapi/arnu/ARNUexporter.java
@@ -1,9 +1,11 @@
 package nl.ovapi.arnu;
 
+import java.io.ByteArrayOutputStream;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.zip.GZIPOutputStream;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Singleton;
@@ -17,6 +19,9 @@ import nl.tt_solutions.schemas.ns.rti._1.ServiceInfoServiceType;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
 
 import com.google.common.collect.Queues;
 
@@ -27,12 +32,18 @@ public class ARNUexporter {
 	private static final Logger _log = LoggerFactory.getLogger(ARNUexporter.class);
 	private ScheduledExecutorService _scheduler;
 	private ConcurrentLinkedQueue<ServiceInfoServiceType> workQueue;
+	private Socket publisher;
+	private Context context;
+	private static boolean FORMAT_XML = true;
 
 	@PostConstruct
 	public void start() {
 		_scheduler = Executors.newScheduledThreadPool(1);
 		_scheduler.scheduleAtFixedRate(new SendTask(), 500, 500, TimeUnit.MILLISECONDS);
 		workQueue = Queues.newConcurrentLinkedQueue();
+		context = ZMQ.context(1);
+		publisher = context.socket(ZMQ.XPUB);
+		publisher.bind("tcp://0.0.0.0:9817");
 	}
 
 	private class SendTask implements Runnable{
@@ -54,17 +65,25 @@ public class ARNUexporter {
 				if (infoList.getServiceInfo().size() == 0){
 					return;
 				}
-				/*Marshaller marshaller = null;
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				GZIPOutputStream gzip = new GZIPOutputStream(out);
+				Marshaller marshaller = null;
 				try {
 					jc = JAXBContext.newInstance(PutServiceInfoIn.class);
 					marshaller = jc.createMarshaller();
-					marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+					marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, FORMAT_XML);
 				} catch (JAXBException e1) {
 					_log.error("Error with JAXB",e1);
 					e1.printStackTrace();
 				}
-				marshaller.marshal(putService, System.out);*/
+				marshaller.marshal(putService, gzip);
+				gzip.close();
+				publisher.sendMore("/OVAPI/ARNURitinfo");
+				publisher.send(out.toByteArray());
+				out.close();
+				gzip.close();
 			} catch (Exception e) {
+				_log.error("ARNU exporter exception",e);
 				e.printStackTrace();
 			}	
 		}
