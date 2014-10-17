a84d30c4561f3c7cd084390d5fd63381dbe8c543
Thomas Koch <thakoch@gmail.com>
2014-10-17 10:45:35 +0200

    Move test resources that caused NPE's

--- src/test/java/nl/ovapi/bison/sax/KV6Test.java
+++ src/test/java/nl/ovapi/bison/sax/KV6Test.java
@@ -25,22 +25,22 @@ import org.xml.sax.XMLReader;
 
 public class KV6Test {
 
-	@Test
-	public void test1()  throws ParserConfigurationException, SAXException, FileNotFoundException, IOException{
-		SAXParserFactory spf = SAXParserFactory.newInstance();
-		spf.setNamespaceAware(true);
-		SAXParser sp = spf.newSAXParser();
-		XMLReader xr = sp.getXMLReader();
-		KV6SAXHandler handler = new KV6SAXHandler();
-		xr.setContentHandler(handler);
-		URL url = this.getClass().getResource("kv6-3.xml");
-		File f = new File(url.getFile());
-		xr.parse(new InputSource(new FileInputStream(f)));
-		ArrayList<KV6posinfo> posinfos = handler.getPosinfos();
-		assertTrue(posinfos.size() == 3);
-		KV6posinfo posinfo = posinfos.get(0);
-		assertTrue(posinfo.getMessagetype() == Type.DEPARTURE);
-		assertTrue(posinfo.getPunctuality() == 205);
-		assertEquals(1375802845L,posinfo.getTimestamp().longValue());
-	}
+    @Test
+    public void test1() throws ParserConfigurationException, SAXException, FileNotFoundException, IOException {
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        spf.setNamespaceAware(true);
+        SAXParser sp = spf.newSAXParser();
+        XMLReader xr = sp.getXMLReader();
+        KV6SAXHandler handler = new KV6SAXHandler();
+        xr.setContentHandler(handler);
+        URL url = this.getClass().getResource("kv6-3.xml");
+        File f = new File(url.getFile());
+        xr.parse(new InputSource(new FileInputStream(f)));
+        ArrayList<KV6posinfo> posinfos = handler.getPosinfos();
+        assertTrue(posinfos.size() == 3);
+        KV6posinfo posinfo = posinfos.get(0);
+        assertTrue(posinfo.getMessagetype() == Type.DEPARTURE);
+        assertTrue(posinfo.getPunctuality() == 205);
+        assertEquals(1375802845L, posinfo.getTimestamp().longValue());
+    }
 }
