bcef2033c5aa2e55bc520c2e1cf3b98740cb1627
Thomas Koch <thakoch@gmail.com>
2014-11-06 21:11:00 +0100

    Fix test constraints

--- src/test/java/nl/ovapi/bison/sax/KV6Test.java
+++ src/test/java/nl/ovapi/bison/sax/KV6Test.java
@@ -98,10 +98,9 @@ public class KV6Test {
         ArrayList<KV6posinfo> posinfos = handler.getPosinfos();
         assertEquals(1,posinfos.size());
         KV6posinfo posinfo = posinfos.get(0);
-        assertEquals(Type.OFFROUTE,posinfo.getMessagetype());
-        assertEquals(105,posinfo.getPunctuality().intValue());
+        assertEquals(Type.OFFROUTE, posinfo.getMessagetype());
         Assert.assertNull(posinfo.getRd_x());
         Assert.assertNull(posinfo.getRd_y());
-        assertEquals(1415281494, posinfo.getTimestamp().longValue());
+        assertEquals(1415302308, posinfo.getTimestamp().longValue());
     }
 }
